c3b62781b079a943837d743b870007172138e3ab
AydenAG <66919082+AydenAG@users.noreply.github.com>
2024-10-08 19:23:26 +0200

    re set the code

--- Archive coms/js/scripts.js
+++ Archive coms/js/scripts.js
@@ -1,56 +1,25 @@
-/*!
-    * Start Bootstrap - Agency v6.0.3 (https://startbootstrap.com/theme/agency)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
-    */
-    (function ($) {
-    "use strict"; // Start of use strict
+document.addEventListener('DOMContentLoaded', function() {
+    // Event listener for the send button
+    document.getElementById('sendButton').addEventListener('click', function() {
+        const email = document.getElementById('emailInput').value;
+        const message = document.getElementById('messageInput').value;
 
-    // Smooth scrolling using jQuery easing
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-        if (
-            location.pathname.replace(/^\//, "") ==
-                this.pathname.replace(/^\//, "") &&
-            location.hostname == this.hostname
-        ) {
-            var target = $(this.hash);
-            target = target.length
-                ? target
-                : $("[name=" + this.hash.slice(1) + "]");
-            if (target.length) {
-                $("html, body").animate(
-                    {
-                        scrollTop: target.offset().top - 72,
-                    },
-                    1000,
-                    "easeInOutExpo"
-                );
-                return false;
-            }
+        if (email && message) {
+            displayMessage(email, message);
+            document.getElementById('messageInput').value = ''; // Clear input
         }
     });
 
-    // Closes responsive menu when a scroll trigger link is clicked
-    $(".js-scroll-trigger").click(function () {
-        $(".navbar-collapse").collapse("hide");
-    });
+    // Function to display messages in the chat box
+    function displayMessage(email, message) {
+        const chatBox = document.getElementById('chatBox');
 
-    // Activate scrollspy to add active class to navbar items on scroll
-    $("body").scrollspy({
-        target: "#mainNav",
-        offset: 74,
-    });
+        const msgDiv = document.createElement('div');
+        msgDiv.className = 'message ' + (email === 'you@example.com' ? 'user' : 'other');
+        msgDiv.textContent = `${email}: ${message}`;
 
-    // Collapse Navbar
-    var navbarCollapse = function () {
-        if ($("#mainNav").offset().top > 100) {
-            $("#mainNav").addClass("navbar-shrink");
-        } else {
-            $("#mainNav").removeClass("navbar-shrink");
-        }
-    };
-    // Collapse now if page is not at top
-    navbarCollapse();
-    // Collapse the navbar when page is scrolled
-    $(window).scroll(navbarCollapse);
-})(jQuery); // End of use strict
+        chatBox.appendChild(msgDiv);
+        chatBox.scrollTop = chatBox.scrollHeight; // Scroll to bottom
+    }
+});
+
